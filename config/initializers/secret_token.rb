4ef39f62927b06f7b3adac53eeaaa7b6a17be058
Rodney Blevins <rodney@blevins.nl>
2014-08-19 10:34:57 -0600

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Medeor::Application.config.secret_key_base = '9fcfc4bf36a6618eabb5f02d4ebdbcc87211870c8b51875f6dcc4e6cd8126ad4c9808443b7c2470dda80ef572fb783f9d7e692b842bcf3b4b4f2b92db876fdf1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Medeor::Application.config.session_store :cookie_store, key: '_medeor_session'
